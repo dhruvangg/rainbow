3e4db39fc8e6c3d9563684944cae7ca24a9c0167
dhruvang <dhruvangg@gmail.com>
2024-07-08 11:49:21 +0530

    updated menu and UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,156 +1,7 @@
 import { useEffect } from "react"
 import { useState } from "react"
 import { useRef } from "react"
-
-const menu = [{
-  category: 'American Thickshake',
-  image: './assets/falooda.png',
-  color: '#751daa',
-  items: [
-    { name: "Vanilla", price: 90, qty: '250ml' },
-    { name: "Strawberry", price: 90, qty: '250ml' },
-    { name: "Pineapple", price: 90, qty: '250ml' },
-    { name: "Chickoo", price: 90, qty: '250ml' },
-    { name: "Coee Caramel", price: 90, qty: '250ml' },
-    { name: "Plain Chocolate", price: 90, qty: '250ml' },
-    { name: "Bubble Gum", price: 90, qty: '250ml' },
-    { name: "Mango", price: 90, qty: '250ml' },
-    { name: "Chickoo Chocolate", price: 90, qty: '250ml' },
-    { name: "Oreo Cookie (Cream)", price: 90, qty: '250ml' },
-    { name: "Paan Masala", price: 90, qty: '250ml' },
-    { name: "Gulkand", price: 90, qty: '250ml' },
-    { name: "Buer Scotch", price: 90, qty: '250ml' },
-    { name: "Vanilla Chips", price: 90, qty: '250ml' },
-    { name: "Rose Malai", price: 90, qty: '250ml' },
-    { name: "Mango Malai", price: 90, qty: '250ml' },
-    { name: "Kaju Draksh", price: 90, qty: '250ml' },
-    { name: "Chocolate Chips", price: 90, qty: '250ml' },
-    { name: "Black Current", price: 90, qty: '250ml' },
-    { name: "Kaju Anjeer", price: 90, qty: '250ml' },
-    { name: "Kaju Badam", price: 90, qty: '250ml' },
-    { name: "Mava Malai", price: 90, qty: '250ml' },
-    { name: "Almond Caramel", price: 90, qty: '250ml' },
-    { name: "Kaju Gulkand", price: 140, qty: '250ml' },
-    { name: "Kaju Mango", price: 140, qty: '250ml' },
-    { name: "Kesar Pista", price: 140, qty: '250ml' },
-    { name: "Kaju Khajur", price: 140, qty: '250ml' },
-    { name: "Choco Crunch", price: 140, qty: '250ml' },
-    { name: "Gulab Masti", price: 140, qty: '250ml' },
-    { name: "Nuy Nutella", price: 140, qty: '250ml' },
-    { name: "Kit Kat", price: 140, qty: '250ml' },
-    { name: "Dairy Milk", price: 140, qty: '250ml' },
-    { name: "Walnut Chocolate", price: 140, qty: '250ml' },
-    { name: "Fresh Kiwi", price: 140, qty: '250ml' },
-    { name: "Fresh Sitafal", price: 140, qty: '250ml' },
-    { name: "Crunchy Red Velvet", price: 140, qty: '250ml' },
-    { name: "Roasted Almond", price: 140, qty: '250ml' },
-    { name: "Kaju Chocolate", price: 140, qty: '250ml' },
-    { name: "Dryfruit", price: 140, qty: '250ml' },
-    { name: "Fresh Strawberry", price: 140, qty: '250ml' },
-    { name: "Pista Malai", price: 140, qty: '250ml' },
-    { name: "Rajbhog", price: 140, qty: '250ml' },
-    { name: "Lotus Bisco", price: 140, qty: '250ml' },
-    { name: "Kaju Coconut", price: 140, qty: '250ml' },
-    { name: "Kaju Katri", price: 150, qty: '250ml' },
-    { name: "Choco Brownie", price: 160, qty: '250ml' },
-    { name: "Maakhan Bhog", price: 160, qty: '250ml' },
-    { name: "Rajwadi Kaju", price: 160, qty: '250ml' },
-    { name: "Kaju Malai Tukda", price: 170, qty: '250ml' },
-    { name: "Kaju Anjeer Tukda", price: 170, qty: '250ml' },
-    { name: "Kaju Badam Tukda", price: 170, qty: '250ml' },
-    { name: "Fresh Mango", price: 170, qty: '250ml' },
-    { name: "American Dryfruit", price: 170, qty: '250ml' },
-    { name: "Rajwadi Dryfruit", price: 170, qty: '250ml' },
-    { name: "Rocher Ferrero", price: 170, qty: '250ml' },
-    { name: "Dryfruit Khazana", price: 180, qty: '250ml' },
-  ]
-}, {
-  category: 'Special Falooda',
-  color: '#2687ab',
-  items: [
-    { name: "Mastani", price: 70, qty: '250ml' },
-    { name: "Shalimar", price: 80, qty: '250ml' },
-    { name: "Rose Falooda", price: 120, qty: '300ml' },
-    { name: "Mango Falooda", price: 140, qty: '300ml' },
-    { name: "Strawberry Falooda", price: 140, qty: '300ml' },
-    { name: "Green Pista Falooda", price: 140, qty: '300ml' },
-    { name: "Chocolate Falooda", price: 140, qty: '300ml' },
-    { name: "Red Rose Falooda", price: 150, qty: '300ml' },
-    { name: "Black Current Falooda", price: 150, qty: '300ml' },
-    { name: "Badam Pista Falooda", price: 150, qty: '300ml' },
-    { name: "Kaju Anjeer Falooda", price: 150, qty: '300ml' },
-    { name: "Kesar Pista Falooda", price: 150, qty: '300ml' },
-    { name: "Sitafal Falooda", price: 160, qty: '300ml' },
-    { name: "Rajwadi Falooda", price: 170, qty: '300ml' },
-    { name: "Rajbhog Falooda", price: 170, qty: '300ml' },
-  ]
-}, {
-  category: "Fresh Juice",
-  color: '#ad6d00',
-  items: [
-    { name: "Mosambi", price: 60, qty: '250ml' },
-    { name: "Watermelon", price: 60, qty: '250ml' },
-    { name: "Pineapple", price: 70, qty: '250ml' },
-    { name: "Orange", price: 80, qty: '300ml' },
-    { name: "Jambu", price: 120, qty: '300ml' },
-    { name: "Kiwi", price: 120, qty: '300ml' },
-    { name: "Apple", price: 120, qty: '300ml' },
-    { name: "Dadam", price: 120, qty: '300ml' },
-    { name: "Mango (Seasonal)", price: 120, qty: '300ml' },
-    { name: "Strawberry (Seasonal)", price: 120, qty: '300ml' },
-  ]
-}, {
-  category: 'Smoothies Primium Milkshake',
-  color: '#b5437b',
-  items: [
-    { name: "Vanilla", price: 120, qty: '300ml' },
-    { name: "Bubblegum", price: 140, qty: '300ml' },
-    { name: "Oreo Cookies", price: 140, qty: '300ml' },
-    { name: "Fresh Chickoo", price: 150, qty: '300ml' },
-    { name: "Fresh Strawberry", price: 160, qty: '300ml' },
-    { name: "Mango Milk", price: 160, qty: '300ml' },
-    { name: "Black Current", price: 160, qty: '300ml' },
-    { name: "Chocolate Chips", price: 160, qty: '300ml' },
-    { name: "Fresh Guava", price: 170, qty: '300ml' },
-    { name: "Fresh Sitafal", price: 170, qty: '300ml' },
-    { name: "Coee Mocha", price: 170, qty: '300ml' },
-    { name: "Blueberry", price: 170, qty: '300ml' },
-    { name: "KitKat", price: 170, qty: '300ml' },
-    { name: "Kaju Anjeer", price: 180, qty: '300ml' },
-    { name: "Dairy Milk", price: 180, qty: '300ml' },
-    { name: "Dark Chocolate", price: 180, qty: '300ml' },
-    { name: "Choco Brownie", price: 180, qty: '300ml' },
-    { name: "Nutella", price: 180, qty: '300ml' },
-    { name: "Walnut Chocolate Fudge", price: 180, qty: '300ml' },
-    { name: "Avocado", price: 220, qty: '300ml' },
-    { name: "Rocher Ferrero", price: 220, qty: '300ml' },
-    { name: "Lotus Bisco", price: 220, qty: '300ml' },
-  ]
-}, {
-  category: 'Cold Coco',
-  color: '#8f56b3',
-  items: [
-    { name: "Cold Coco", price: 70, qty: '250ml' },
-    { name: "Icecream Coco", price: 100, qty: '300ml' },
-    { name: "Cadbury Coco", price: 100, qty: '300ml' },
-    { name: "Nutella Coco", price: 120, qty: '300ml' },
-    { name: "Kaju Coco", price: 120, qty: '300ml' },
-    { name: "Malai Coco", price: 120, qty: '300ml' },
-    { name: "Rajwadi Coco", price: 170, qty: '300ml' },
-    { name: "Coco ( 1 litre )", price: 200, qty: '300ml' },
-  ]
-}, {
-  category: 'Lassi',
-  color: '',
-  items: [
-    { name: "Lassi", price: 50, qty: '250ml' },
-    { name: "Rose Lassi", price: 60, qty: '300ml' },
-    { name: "Mango Lassi", price: 70, qty: '300ml' },
-    { name: "Malai Lassi", price: 80, qty: '300ml' },
-    { name: "Kaju Lassi", price: 80, qty: '300ml' },
-    { name: "Rajwadi Lassi ", price: 120, qty: '300ml' },
-  ]
-}]
+import { menu } from "./menu"
 
 export default function App() {
   const [activeMenuItem, setActiveMenuItem] = useState(null)
@@ -215,13 +66,16 @@ export default function App() {
                   <h3 className="font-semibold text-2xl my-4">{category}</h3>
                 </div>
                 <ul>
-                  {items.map(({ name, price, qty }) => <li key={name} className="border-gray-400 flex flex-row py-2 border-b border-gray-100">
-                    <div className="select-none cursor-pointer flex flex-1 items-center">
-                      <div className="flex-1 mr-16">
-                        <div className="font-medium">{name}</div>
-                        <div className="text-gray-600 text-xs">{qty}</div>
-                      </div>
-                      <div className="text-lg font-semibold">{price}</div>
+                  {items.map(({ name, notes, rate }) => <li key={name} className="grid grid grid-cols-5 gap-4 p-2 mb-5 rounded-lg shadow">
+                    <div className="col-span-3">
+                      <div className="font-medium">{name}</div>
+                      <div className="text-gray-600 text-xs">{notes}</div>
+                    </div>
+                    <div className="col-span-2 text-right flex">
+                      {rate.map(({ price, qty }, i) => <div key={i} className="flex-1">
+                        <div className="text-lg font-semibold">{price}</div>
+                        <div className="text-xs font-semibold" style={{ whiteSpace: "pre-wrap" }}>{qty}</div>
+                      </div>)}
                     </div>
                   </li>)}
                 </ul>
